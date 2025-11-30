4afabe4da362020fcc37d3d7fd7e9eebb704ec28
Lebanvla <antonlebedev2004@yandex@ru>
2025-11-30 14:02:14 +0300

    Add function getExcerpt

--- logic/common_entities.php
+++ logic/common_entities.php
@@ -1,12 +1,34 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $bd = new PDO("mysql:dbname=todo_list;host=127.0.0.1", "root", "qq");
-$is_authorised = isset($_SESSION["id"]);
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+$is_authorised = isset($_SESSION["id"]);
 
 function redirect(string $location)
 {
     header("Location: $location");
     exit;
+}
+
+
+function getExcerpt($text, $maxLength = 50, $encoding = 'UTF-8')
+{
+    // Обрезаем до максимальной длины
+    if (mb_strlen($text, $encoding) <= $maxLength) {
+        return $text;
+    }
+
+    $excerpt = mb_substr($text, 0, $maxLength, $encoding);
+
+    // Находим последний пробел (уже с учётом многобайтовости)
+    $lastSpace = mb_strrpos($excerpt, ' ', 0, $encoding);
+    if ($lastSpace !== false) {
+        $excerpt = mb_substr($excerpt, 0, $lastSpace, $encoding);
+    }
+
+    return $excerpt . '...';
 }
